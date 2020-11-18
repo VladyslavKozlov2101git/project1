9af8815723134f668f88f1479ebef6d173f96ae2
Vladyslav <vladislavkozlov2101git@gmail.com>
2020-11-18 20:59:08 +0200

    added leesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header (){
+    return <header className="header">
+    <img src= "https://i.pinimg.com/originals/b1/85/9b/b1859bdac7d42d4a0a5a673bc4265564.png"></img>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Profile() {
+    return <div className="Profile">
+    <div>
+      <img           
+        src= "https://images11.esquire.ru/upload/img_cache/813/81312fe17cd8a6fc9e963cb67fc9998b_ce_3000x1871x0x134_cropped_960x600.jpg">
+      </img>
+    </div>
+
+    <div>
+      ava+description
+    </div>
+
+    <div>
+      My post
+      <div>
+          New post
+      </div>
+      <div>
+          Post 1
+      </div>
+      <div>
+          Post 2
+      </div>
+    </div>
+    
+  </div>
+
+
+}
+
+export default Profile;
