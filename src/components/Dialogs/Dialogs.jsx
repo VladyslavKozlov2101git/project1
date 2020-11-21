10915987e747238f117b0cdbaafb8725a6b90fab
Vladyslav <vladislavkozlov2101git@gmail.com>
2020-11-21 21:37:26 +0200

    +

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import c from './Dialogs.module.css'
+
+
+
+function Dialogs(props) {
+    return <div className="Dialogs">    
+        Dialogs
+     </div>
+
+
+}
+
+export default Dialogs;
